b036e483fbfff651b153032f88e8859aba7db79a
MashaTelyatnikova <mashatelatnikova@gmail.com>
2014-11-01 17:49:15 +0500

    01 Task

--- CleanCode/01_ExtractMethods.cs
+++ CleanCode/01_ExtractMethods.cs
@@ -5,26 +5,31 @@ namespace CleanCode
 {
 	public static class RefactorMethod
 	{
-		private static void SaveData(string s, byte[] d)
+		private static void SaveData(string file, byte[] data)
 		{
-			//open files
-			var fs1 = new FileStream(s, FileMode.OpenOrCreate);
-			var fs2 = new FileStream(Path.ChangeExtension(s, "bkp"), FileMode.OpenOrCreate);
 
-			// write data
-			fs1.Write(d, 0, d.Length);
-			fs2.Write(d, 0, d.Length);
+		    var fileStream = GetFileStream(file, Path.GetExtension(file));
+		    var backupFileStream = GetFileStream(file, "bkp");
+			
+            WriteDataToFileStream(data, fileStream);
+            WriteDataToFileStream(data, backupFileStream);
 
-			// close files
-			fs1.Close();
-			fs2.Close();
-
-			// save last-write time
-			string tf = s + ".time";
-			var fs3 = new FileStream(tf, FileMode.OpenOrCreate);
-			var t = BitConverter.GetBytes(DateTime.Now.Ticks);
-			fs3.Write(t, 0, t.Length);
-			fs3.Close();
+			var fileTime = file + ".time";
+		    var timeFileStream = GetFileStream(fileTime, Path.GetExtension(fileTime));
+			var bytesCurrentTime = BitConverter.GetBytes(DateTime.Now.Ticks);
+			
+            WriteDataToFileStream(bytesCurrentTime, timeFileStream);
 		}
+
+	    public static FileStream GetFileStream(string path, string extension)
+	    {
+	        return new FileStream(Path.ChangeExtension(path, extension), FileMode.OpenOrCreate );
+	    }
+
+	    public static void WriteDataToFileStream(byte[] data, FileStream fileStream)
+	    {
+	        fileStream.Write(data, 0, data.Length);
+            fileStream.Close();
+	    }
 	}
 }
